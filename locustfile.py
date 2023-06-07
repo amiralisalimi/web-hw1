a69892325cfcc3700f6345e22b08f1f7be589393
amirali <me.amiralisalimi@gmail.com>
2023-06-08 00:35:01 +0330

    Add load-test using locust

--- locustfile.py
+++ locustfile.py
@@ -3,4 +3,14 @@ from locust import HttpUser, task
 class HelloWorldUser(HttpUser):
     @task
     def auth(self):
-        self.client.get('localhost:5052/auth')
+        self.client.get('/auth')
+    @task
+    def get_users(self):
+        self.client.post(
+            '/get-users',
+            json={
+                'authKey': '31',
+                'userId': '0',
+                'withSqlInject': False,
+            }
+        )
--- sql/web_hw1.sql
+++ sql/web_hw1.sql
@@ -9,4 +9,9 @@ CREATE TABLE USERS (
     createdAt TIMESTAMP WITH TIME ZONE
 );
 
+INSERT INTO USERS (name, family, id, age, sex, createdAt) VALUES
+    ('amir', 'nejad', 1, 18, 'male', '2023-06-07 23:53:54.325440209 +0330'),
+    ('ali', 'salim', 2, 20, 'male', '2022-06-07 23:53:54.325440209 +0330'),
+    ('fati', 'paydar', 3, 22, 'female', '2023-06-06 23:53:52.325440209 +0330');
+
 COMMIT;
